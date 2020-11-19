cbe5788c1f02babadfecb57c78e69c515ea6bfbc
michael.fallorina@gmail.com <michael.fallorina@gmail.com>
2020-11-18 18:40:36 -0800

    attempting to fix cors error

--- src/StreamingOptions/StreamingOptions.js
+++ src/StreamingOptions/StreamingOptions.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
+import './StreamingOptions.css';
 
 class StreamingOptions extends Component {
   render() {
     return (
-      <div className="content-streaming overview-section">
+      <div className="content-streaming overview-section flex">
         <fieldset>
           <div>
-            <label htmlFor="is_netflix" />Netflix
-            <input type="checkbox" id="is_netflix" name="option" value="Netflix"  />
+            <label htmlFor="is_netflix">Netflix
+            <input type="checkbox" id="is_netflix" name="option" value="Netflix" /></label>
           </div>
           <div>
-            <label htmlFor="is_hulu" />Hulu
-            <input type="checkbox" id="is_hulu" name="option" value="Hulu"  />
+            <label htmlFor="is_hulu">Hulu
+            <input type="checkbox" id="is_hulu" name="option" value="Hulu" /></label>
           </div>
           <div>
-            <label htmlFor="is_prime" />Prime Video
-            <input type="checkbox" id="is_prime" name="option" value="Prime Video"  />
+            <label htmlFor="is_prime">Prime Video
+            <input type="checkbox" id="is_prime" name="option" value="Prime Video" /></label>
           </div>
         </fieldset>
       </div>
